202e05b62eedf46c91469998bb648fbc5963a30d
Justin Muskopf <jaymuskopf@gmail.com>
2019-01-19 18:40:51 -0600

    Remove ENT from SENTS

--- lua/cfc_m9k_stubber/stubs/m9k_specialties/m9k_ammo_ieds.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_specialties/m9k_ammo_ieds.lua
@@ -3,6 +3,6 @@ if SERVER then AddCSLuaFile() end
 CFC_M9k_Stubber.registerStub(function()
     local sent = scripted_ents.GetStored("m9k_ammo_ieds").t
 
-    sent.ENT.Spawnable = false
-    sent.ENT.AdminOnly = true
+    sent.Spawnable = false
+    sent.AdminOnly = true
 end)
--- lua/cfc_m9k_stubber/stubs/m9k_specialties/m9k_ammo_proxmines.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_specialties/m9k_ammo_proxmines.lua
@@ -3,5 +3,5 @@ if SERVER then AddCSLuaFile() end
 CFC_M9k_Stubber.registerStub(function()
     local sent = scripted_ents.GetStored("m9k_ammo_proxmines").t
 
-    sent.ENT.AdminOnly = true
+    sent.AdminOnly = true
 end)
