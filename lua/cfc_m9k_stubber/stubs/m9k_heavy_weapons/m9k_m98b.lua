30857db96e26bcfd827d9964d0d0f80d22681389
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-07-06 20:12:49 -0600

    m9k M98b redesign
    Changes the m98bs' damage and mag size.
    Damage: Default -> 110
    Mag: Default -> 1

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_m98b.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_m98b.lua
@@ -2,7 +2,9 @@ if SERVER then AddCSLuaFile() end
 
 CFC_M9k_Stubber.registerStub(function()
     local weapon = weapons.GetStored("m9k_m98b")
-
+    
+    weapon.Primary.ClipSize = 1
+    weapon.Primary.Damage = 110
     weapon.Primary.Spread = .07
-    weapon.Primary.IronAccuracy = .002
+    weapon.Primary.IronAccuracy = .001
 end)
