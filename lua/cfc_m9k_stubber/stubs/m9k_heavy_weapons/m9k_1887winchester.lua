d2e4f3c70bc6559a9764a821e8c3a78a2bc533d1
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2019-07-23 19:40:54 -0600

    1887 buff
    Improves damage by 1 (50 total)

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_1887winchester.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_1887winchester.lua
@@ -4,5 +4,5 @@ CFC_M9k_Stubber.registerStub(function()
     local weapon = weapons.GetStored("m9k_1887winchester")
 
     weapon.Primary.RPM = 60
-    weapon.Primary.Damage = 4
+    weapon.Primary.Damage = 5
 end)
