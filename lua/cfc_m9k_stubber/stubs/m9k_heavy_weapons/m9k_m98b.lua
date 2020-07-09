3d502cac73d9e4b2aca52ffdddaad5367fcd1ae9
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2020-07-09 17:57:55 -0600

    Improves m9k m98b/m82 damage.
    big bullet energy.

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_m98b.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_m98b.lua
@@ -4,7 +4,7 @@ CFC_M9k_Stubber.registerStub( function()
     local weapon = weapons.GetStored( "m9k_m98b" )
 
     weapon.Primary.ClipSize = 1
-    weapon.Primary.Damage = 110
+    weapon.Primary.Damage = 1000
     weapon.Primary.Spread = .1
     weapon.Primary.IronAccuracy = .001
 end )
