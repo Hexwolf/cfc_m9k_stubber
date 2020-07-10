4dcfd7924d0cce55ce2e9081d42f2a8431bc952d
PLally <11771225+PLally@users.noreply.github.com>
2020-07-09 20:06:42 -0400

    remove a tab

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_barret_m82.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_barret_m82.lua
@@ -4,7 +4,7 @@ CFC_M9k_Stubber.registerStub( function()
     local weapon = weapons.GetStored( "m9k_barret_m82" )
 
     weapon.Primary.RPM = 50
-	weapon.Primary.Damage = 200
+    weapon.Primary.Damage = 200
     weapon.Primary.Spread = .1
     weapon.Primary.IronAccuracy = .0022
 end )
