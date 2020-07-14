79d09c37b623f2ebb78252e565f3187da9a9035b
Hexwolf <45373549+Hexwolf@users.noreply.github.com>
2020-07-14 16:22:16 -0600

    AMR adjustments.
    Adjusts AMR class m9k snipers, adds AW50 to AMR weapon class.

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_aw50.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_aw50.lua
@@ -2,7 +2,9 @@ if SERVER then AddCSLuaFile() end
 
 CFC_M9k_Stubber.registerStub( function()
     local weapon = weapons.GetStored( "m9k_aw50" )
-
-    weapon.Primary.Spread = .07
-    weapon.Primary.IronAccuracy = .002
+	
+	weapon.PrintName = "AW50 AMR"
+    weapon.Primary.Spread = .1
+	weapon.Primary.Damage = 150
+    weapon.Primary.IronAccuracy = .0020
 end )
